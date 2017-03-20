e5f58a524fbff0eadc44e00bcd984b4082019894
pd2vm <keithchan907@gmail.com>
2017-03-21 02:57:06 +0800

    11

--- project1.cpp
+++ project1.cpp
@@ -5,12 +5,12 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-	srand(time_t(NULL));
 	cout << "1 2 3 4 5 6 7 8" << endl;
 	string zero;
 	cin >> zero;
 	while (1)
 	{
+	srand(time_t(NULL));
 		int a = rand() % 20;
 		int b = rand() % 15;
 		cout << "1 1 "<<a<<" "<<b << endl;
