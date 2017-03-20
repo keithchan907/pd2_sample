d684638f427bbcc7e6b26722d9b48e0dd967ddfd
pd2vm <keithchan907@gmail.com>
2017-03-21 02:25:49 +0800

    1111111111111

--- project1.cpp
+++ project1.cpp
@@ -7,14 +7,11 @@ int main(int argc, char *argv[])
 	cout << "1 2 3 4 5 6 7 8" << endl;
 	string zero;
 	cin >> zero;
-	while (1)
-	{
 while(1)
 {
 		cout << "1 1 1 1"<<endl;
 }
 		cout << 0 << endl;
 cin>>zero;
-	}
 		return 0;
 }
