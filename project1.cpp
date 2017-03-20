78025d9d946550466040b9003a91fce1cba65598
pd2vm <keithchan907@gmail.com>
2017-03-21 02:31:30 +0800

    1

--- project1.cpp
+++ project1.cpp
@@ -7,11 +7,13 @@ int main(int argc, char *argv[])
 	cout << "1 2 3 4 5 6 7 8" << endl;
 	string zero;
 	cin >> zero;
-while(1)
-{
-		cout << "1 1 1 1"<<endl;
-}
-		cout << 0 << endl;
-cin>>zero;
-		return 0;
+	int n = 0;
+	while ( n < 20)
+	{
+		cout << "1 1 1 1" << endl;
+		n = n + 1;
+	}
+	cout << 0 << endl;
+	cin >> zero;
+	return 0;
 }
