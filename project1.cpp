11e79910b6f3acfe4f8d0102aafac36a07d8acac
pd2vm <keithchan907@gmail.com>
2017-03-21 02:34:28 +0800

    11

--- project1.cpp
+++ project1.cpp
@@ -11,6 +11,13 @@ int main(int argc, char *argv[])
 	while ( n < 20)
 	{
 		cout << "1 1 1 1" << endl;
+		cout << "2 1 1 1" << endl;
+		cout << "3 1 1 1" << endl;
+		cout << "4 1 1 1" << endl;
+		cout << "5 1 1 1" << endl;
+		cout << "6 1 1 1" << endl;
+		cout << "7 1 1 1" << endl;
+		cout << "8 1 1 1" << endl;
 		n = n + 1;
 	}
 	cout << 0 << endl;
