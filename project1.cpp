c9c27635613d2c4935aa729a6ae87027316453b3
pd2vm <keithchan907@gmail.com>
2017-03-21 02:43:34 +0800

    11

--- project1.cpp
+++ project1.cpp
@@ -8,12 +8,12 @@ int main(int argc, char *argv[])
 	string zero;
 	cin >> zero;
 		cout << "1 1 1 1" << endl;
-		cout << "2 1 1 1" << endl;
-		cout << "3 1 1 1" << endl;
-		cout << "4 1 1 1" << endl;
-		cout << "5 1 1 1" << endl;
-		cout << "6 1 1 1" << endl;
-		cout << "7 1 1 1" << endl;
-		cout << "8 1 1 1" << endl;
+		cout << "1 8 1 1" << endl;
+		cout << "1 7 1 1" << endl;
+		cout << "1 6 1 1" << endl;
+		cout << "1 5 1 1" << endl;
+		cout << "1 4 1 1" << endl;
+		cout << "1 3 1 1" << endl;
+		cout << "1 2 1 1" << endl;
 	cout << 0 << endl;
 }
