203dcd5dd98d5b619c67735ce618c90b988b99e7
pd2vm <keithchan907@gmail.com>
2017-03-21 02:01:44 +0800

    111111

--- project1.cpp
+++ project1.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-int zero;
-cin>>zero;
+	string zero;
+	cin >> zero;
 	cout << "1 2 3 4 5 6 7 8" << endl;
 	while (1)
 	{
 
-			cout << "1 1" << rand() % 20 + 1 << " " << rand() % 15 + 1 << endl;
+		cout << "1 1" << rand() % 20 + 1 << " " << rand() % 15 + 1 << endl;
 		cout << 0 << endl;
 		;
 		return 0;
