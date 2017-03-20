74785bbf76fae67e58433e7dab24de12fbe5c1b6
pd2vm <keithchan907@gmail.com>
2017-03-21 01:50:43 +0800

    111111

--- project1.cpp
+++ project1.cpp
@@ -4,7 +4,11 @@ using namespace std;
 int main(int argc, char *argv[])
 {
 	cout << "1 2 3 4 5 6 7 8" << endl;
-	while(1);
+	while(1)
+{
+cout<<"1 1"<<rand()20%+1<<" "<<rand()15%+1<<endl;
+cout<<0<<endl;
+;
 	return 0;
 }
 
