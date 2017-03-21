46241dc1fbbde7bda53221798b469eff8cb5b7fd
pd2vm <keithchan907@gmail.com>
2017-03-21 11:04:52 +0800

    1

--- project1.cpp
+++ project1.cpp
@@ -8,20 +8,36 @@ using namespace std;
 int main(int argc, char *argv[])
 {
 	cout << "1 2 3 4 5 6 7 8" << endl;
-	int zero;
-	cin >> zero;
+	int gameCountdown; 
+   int mana ;
+        int card1;
+         int card2;
+        int card3;
+        int card4;
+	cin >> gameCountdown;
+	cin >> mana;	
+cin >> card1;	
+cin >> card2;	
+cin >> card3;
+cin>>card4;
+int num[4]={card1,card2,card3,card4};
+for(int j=0;j<4;j++)
+{
+if(num[j]>num[j+1])
+{
+int k=num[j];
+num[j]=num[j-1];
+num[j-1]=k;
+}
+}
 	while (1)
 	{
 		int a = rand() % 20+1;
 		int b = rand() % 12+8;
-		cout << "1 1 "<<a<<" "<<b << endl;
-		cout << "1 2 " << a << " " << b << endl;
-		cout << "1 3 " << a << " " << b << endl;
-		cout << "1 4 " << a << " " << b << endl;
-		cout << "1 5 " << a << " " << b << endl;
-		cout << "1 6 " << a << " " << b << endl;
-		cout << "1 7 " << a << " " << b << endl;
-		cout << "1 8 " << a << " " << b << endl;
+		cout << "1 "<<num[3] <<a<<" "<<b << endl;
+		cout << "1 "<<num[2] << a << " " << b << endl;
+		cout << "1 "<<num[1]  << a << " " << b << endl;
+		cout << "1 "<<num[0]  << a << " " << b << endl;
 		cout << 0 << endl;
 	}
 }
